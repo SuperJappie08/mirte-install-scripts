52b68a078951d9b9184ae75602eb7fb91902faad
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2025-07-10 11:14:49 +0200

    Install pico-py-serial-flash@cli via pipx

--- install_arduino.sh
+++ install_arduino.sh
@@ -53,8 +53,10 @@ sudo make install
 cd $MIRTE_SRC_DIR/mirte-telemetrix4rpipico || exit 1
 git submodule update --init --recursive
 
-pip install -U "pip>=25" || true                                         # pico-py-serial-flash requires a newer version of pip, otherwise it'll be installed as UNKNOWN package
-pip install git+https://github.com/arendjan/pico-py-serial-flash.git@cli # uart flashing utility when using the pcb
+sudo apt install pipx
+# pip install -U "pip>=25" || true                                         # pico-py-serial-flash requires a newer version of pip, otherwise it'll be installed as UNKNOWN package
+# pip install git+https://github.com/arendjan/pico-py-serial-flash.git@cli # uart flashing utility when using the pcb
+pipx install git+https://github.com/arendjan/pico-py-serial-flash.git@cli
 
 cd $MIRTE_SRC_DIR/mirte-install-scripts/
 # Already build all versions so only upload is needed *don't do for all, as it requires loads of space for the tools.
